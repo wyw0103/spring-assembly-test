75c5889e8f6d55e476927b2188c9a56c452bdf0f
Cassius Cai <cassiuscai@gmail.com>
2015-08-06 13:36:45 +0800

    Unit Test Fix

--- usages/src/test/java/com/cassius/spring/assembly/test/testng/NewTestNGWithSpyTest.java
+++ usages/src/test/java/com/cassius/spring/assembly/test/testng/NewTestNGWithSpyTest.java
@@ -1,11 +1,11 @@
 package com.cassius.spring.assembly.test.testng;
 
-import com.cassius.spring.assembly.test.common.setting.configure.SpringContextConfigure;
 import com.cassius.spring.assembly.test.common.setting.processor.SpringBean;
 import com.cassius.spring.assembly.test.usages.domain.Service;
 
 import org.assertj.core.api.Assertions;
 import org.mockito.internal.util.MockUtil;
+import org.springframework.test.context.ContextConfiguration;
 import org.testng.annotations.Test;
 
 /**
@@ -14,9 +14,9 @@ import org.testng.annotations.Test;
  * @author Cassius Cai
  * @version v 0.1 8/5/15 23:53 Exp $
  */
-@SpringContextConfigure({ "META-INF/spring/spring.xml",
-                          "classpath:META-INF/spring/spring-assembly-test-common.xml" })
-public class NewTestNGWithSpyTest extends TestNGSpringAssemblyTest {
+@ContextConfiguration(locations = { "classpath:META-INF/spring/spring.xml",
+                                    "classpath:META-INF/spring/spring-assembly-test-common.xml" })
+public class NewTestNGWithSpyTest extends AbstractTestNGSpringAssemblyTests {
 
     /**
      * The Service.
